6654b5cd255f799146351e20740209e5a44d0f21
F0xR0cket <94502451+F0xR0cket@users.noreply.github.com>
2026-04-27 19:44:53 -0400

    Anxiety Update
    Player can get scared and freeze

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -10,9 +10,14 @@ public class PlayerMovement : MonoBehaviour
     AudioSource m_AudioSource;
     private List<string> m_OwnedKeys = new List<string>();
 
-    public float walkSpeed = 1.0f;
+    public float walkSpeed = 1.33333f;
     public float turnSpeed = 20f;
 
+    public GameObject ScaredUI;
+
+    private bool IsScared = false;
+    private int HowScared = 0;
+    //private int anxiety = 0;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
@@ -23,6 +28,48 @@ public class PlayerMovement : MonoBehaviour
         MoveAction.Enable();
         m_Animator = GetComponent<Animator>();
         m_AudioSource = GetComponent<AudioSource>();
+        InvokeRepeating("Panic",20,40);
+        //HowScared = 5;
+    }
+
+    private void Panic()
+    {
+        if (Random.Range(0, 5) == 5);
+        {
+            HowScared = Random.Range(0, 5);
+        }
+    }
+
+    private void Update()
+    {
+     
+
+        if (HowScared > 0)
+        {
+            IsScared = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                HowScared -= 1;
+                Debug.Log("HowScared is " + HowScared);
+            }
+
+        }
+        else
+        {
+            IsScared = false;
+        }
+        
+
+        if (IsScared)
+        {
+            MoveAction.Disable();
+            ScaredUI.SetActive(true);
+        }
+        else
+        {
+            MoveAction.Enable();
+            ScaredUI.SetActive(false);
+        }
     }
 
     void FixedUpdate()
@@ -32,12 +79,15 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = pos.x;
         float vertical = pos.y;
 
+        
+
+
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        bool isWalking = hasHorizontalInput || hasVerticalInput && IsScared != true;
         m_Animator.SetBool("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
@@ -45,6 +95,9 @@ public class PlayerMovement : MonoBehaviour
 
         m_Rigidbody.MoveRotation(m_Rotation);
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+
+
+
 
         if (isWalking)
         {
@@ -68,4 +121,7 @@ public class PlayerMovement : MonoBehaviour
     {
         return m_OwnedKeys.Contains(keyName);
     }
+
+
+
 }
